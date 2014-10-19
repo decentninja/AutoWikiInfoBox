6683d11e7e34399c6a45fc40f0f89204fb1d82a6
Viktor Palmkvist <viktor@palmkvist.com>
2014-10-19 14:28:03 +0200

    Counting pretty much done
    Note that if the pattern is found within the AROUND words from another pattern
    only one will be counted correctly, the other will end up in postfix.

--- parse.py
+++ parse.py
@@ -1,5 +1,7 @@
 import json
 import sys
+import re
+import string
 
 
 def warning(str):
@@ -13,7 +15,31 @@ AROUND = 3
 property = sys.argv[1]
 type = sys.argv[2]
 
+allcount = {}
 allpostfix = {}		# word: count
+allprefix = {}
+allcorrect = {}
+allbackground = {}
+
+stripNonAlphaNumRe = re.compile('[^\w \d]+')
+
+def addWord(word, countmap, globalcountmap=None):
+	if word in countmap:
+		countmap[word] += 1
+	else:
+		countmap[word] = 1
+	if not globalcountmap:
+		return
+	if word in globalcountmap:
+		globalcountmap[word] += 1
+	else:
+		globalcountmap[word] = 1
+
+def addWordOrNum(won, countmap, globalcountmap=None):
+	if won.isalpha():
+		addWord(won, countmap, globalcountmap)
+	else:
+		addWord('1', countmap, globalcountmap)
 
 for person in people:
 	things = person[property].split(";")
@@ -24,23 +50,31 @@ for person in people:
 	elif type == "string":
 		pass
 	else:
-		warning("Unknown type. Select date or link")
+		warning("Unknown type")
 	things = list(set(things))
 	text = person["description_en"]
+	text = stripNonAlphaNumRe.sub('', text).lower()
 	for thing in things:
 		try:
-			index = text.index(thing)
-			prefix = text[0:index]
-			postfix = text[index + len(thing):]
-			prefixwords = prefix.split(" ")[AROUND:]
-			postfix = postfix.split(" ")[:AROUND]
-			for word in postfix:
-				word = word.lower()
-				if word in allpostfix:
-					allpostfix[word] += 1
-				else:
-					allpostfix[word] = 1
+			foundCount = 0
+			while True:
+				index = text.index(thing)
+				prefix = text[0:index]
+				text = text[index + len(thing):]
+				prefix = prefix.split(" ")
+				postfix = text.split(" ")[:AROUND]
+				text = text[" ".join(postfix).__len__:]
+				foundCount += 1
+				for word in postfix:
+					addWordOrNum(word, allpostfix, allcount)
+				for word in prefix[-AROUND:]:
+					addWordOrNum(word, allprefix, allcount)
+				for word in prefix[:-AROUND]:
+					addWordOrNum(word, allbackground, allcount)
 		except ValueError:
-			warning("\"" + thing + "\" not found in " + person["name"] + " abstract.")
+			if foundCount == 0:
+				warning("\"" + thing + "\" not found in " + person["name"] + " abstract.")
+		for word in text.split(" "):
+			addWordOrNum(word, allbackground, allcount)
 
-print(allpostfix)
+
