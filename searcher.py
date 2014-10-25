5eeb7e3ca01df635f7500bfeaf0ae81d41252392
sootn <encrypt.wave@gmail.com>
2014-10-25 16:02:48 +0200

    Correct success counting and better debug printing

--- searcher.py
+++ searcher.py
@@ -37,7 +37,7 @@ states = ["back", "pre", "target", "post"]
 hmm = subprocess.Popen(["./viterbi"], stdin=subprocess.PIPE, stdout=subprocess.PIPE)
 hits = 0
 incorrect = 0
-misses = 0
+notfound = 0
 try:
 	kattismatrix(a, hmm.stdin)
 	kattismatrix(b, hmm.stdin)
@@ -49,24 +49,42 @@ try:
 		hmm.stdin.write(str(len(tokens)) + " " + " ".join([str(toState(token, wordlist)) for token in tokens]) + "\n")
 		result = hmm.stdout.readline()
 		result = [int(word) for word in result.split(" ")]
-		value = helpers.extract(person[property], type)
-		sys.stdout.write(value)
-		hasHit = False
+		values = [helpers.extract(x, type) for x in person[property].split(";")]
+		weguessed = False
+		correct = False
+		debug = ""
 		for i, state in enumerate(result):
+			debugstyle = ""
+			if tokens[i] in values:
+				debugstyle += '\033[4m'
+			if state == 1:
+				debugstyle += '\033[90m'
 			if state == 2:
-				hasHit = True
-				sys.stdout.write(" " + tokens[i])
-				if value == tokens[i]:
-					hits += 1
-				else:
-					incorrect += 1
-		if not hasHit:
-			misses += 1
-		sys.stdout.write("\n")
+				debugstyle += '\033[91m'
+			if state == 3:
+				debugstyle += '\033[92m'
+			debug += debugstyle + tokens[i] + '\033[0m' + " "
+
+			if state == 2:
+				weguessed = True
+				ourguess = tokens[i]
+				for value in values:
+					if value == tokens[i]:
+						correct = True
+		debug += "\n"
+		if not weguessed:
+			notfound += 1
+			print("\033[92mNot found: \033[0m" + debug)
+		elif correct:
+			hits += 1
+			print("\033[4m\033[91mCorrect\033[0m: " + debug)
+		else:
+			print("\033[93mIncorrect:\033[0m " + debug)
+			incorrect += 1
 finally:
 	hmm.stdin.close()
 	hmm.terminate()
-print("num people: " + str(len(people)))
-print("hits: " + str(hits))
-print("incorrect: " + str(incorrect))
-print("misses: " + str(misses))
+print("Num people: " + str(len(people)))
+print("Correct: " + str(hits))
+print("Incorrect: " + str(incorrect))
+print("Not found: " + str(notfound))
